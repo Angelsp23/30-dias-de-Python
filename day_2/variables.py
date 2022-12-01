e006ce05e4667327a8eb5f0c61f1d38b40100f8e
Asier55555 <amartre089@m.educaand.es>
2022-12-01 09:16:32 +0100

    dia 2

---
+++ day_2/variables.py
@@ -0,0 +1,30 @@
+#Day 2: 30 Days of python programming
+first_name = "Angel"
+last_name = "Sanchez"
+full_name = "Angel Sanchez"
+country = "Spain"
+city = "Jerez"
+age = 16
+year = 2022
+is_married = False
+is_true = True
+is_light_on = True
+
+first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light_on
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(len(first_name))
+print(len(first_name) == len(last_name))
+
+num_one=5
+num_two=4
+count1 = sum(num_one,num_two)
+
