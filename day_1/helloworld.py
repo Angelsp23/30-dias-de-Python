f639e73ea103b9c9494e842a96cd655146fd74c2
Asier55555 <amartre089@m.educaand.es>
2022-12-01 08:23:24 +0100

    día 1

---
+++ day_1/helloworld.py
@@ -0,0 +1,35 @@
+#level2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+print("Angel")
+print("Sanchez Parra")
+print("Spain")
+print("I am enjoying 30 days of python")
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(["Asabeneh","Python","Finland"]))
+print(type("Angel"))
+print(type("Sanchez Parra"))
+print(type("Spain")) 
+#level3
+print(23)
+print(2.3)
+print(2+3j)
+print("hola mundo")
+print(True)
+print([1,3,5])
+print(("uva","manzana","pera"))
+print({4.3, 3, 5})
+print({
+    "uva" : 2.5,
+    "manzana" : 1.3,
+    "pera" : 2
+})
+
