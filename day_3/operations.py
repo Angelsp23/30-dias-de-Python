636decb42fabaf6b4c2ffe43905f10f1d782c84b
Asier55555 <amartre089@m.educaand.es>
2022-12-02 11:10:20 +0100

    no está terminado, tengo que solucionar algunos problemas

---
+++ day_3/operations.py
@@ -0,0 +1,11 @@
+age = int=16
+height = float=1.85
+ecuacion = complex= 3 + 3j
+base = input ("Area:")
+print(base)
+int= base
+altura = input("Altura:")
+print(altura)
+int= altura
+print ("El perímetro de su triangulo es:")
+print(0.5 * altura * base)
