85df9bc666aad86c5351e587940600b8c735fd9a
Asier55555 <amartre089@m.educaand.es>
2022-12-15 09:18:00 +0100

    incompleto

--- day_3/operations.py
+++ day_3/operations.py
@@ -8,3 +8,34 @@ print(altura)
 print ("El perímetro de su triangulo es:")
 print(0.5 * altura * nbase)
 
+lado_a = int(input("Inserte lado a:"))
+print(lado_a)
+lado_b = int(input("Inserte lado b."))
+print(lado_b)
+lado_c = int(input("Inserte lado c:"))
+print(lado_c)
+perímetro = lado_a + lado_b + lado_c
+print(f"Su perímetro es de: {perímetro}")
+
+lenght = int(input("Inserte el largo:"))
+print(lenght)
+wide = int(input("Inserte el ancho:"))
+print(wide)
+area_rectangle = lenght*wide
+perimeter_rectangle = 2*(lenght + wide)
+print("su área es de:", area_rectangle)
+print("su perímetro es de:", perimeter_rectangle)
+
+pi=3,14
+radio_circle= int(input("Inserte el radio:"))
+print(radio_circle)
+area_circle = pi * radio_circle**2
+circum_circle = 2*pi*radio_circle
+print(area_circle)
+print(circum_circle)
+
+x= int(input("Inserte valor de x"))
+print(x)
+y = 2*x -2
+print("El valor y en 2x-2 es de"y)
+#usar for
