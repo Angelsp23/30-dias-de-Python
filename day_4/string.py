1223b53b49faede9aa81399a2e44ba456211f669
Asier55555 <amartre089@m.educaand.es>
2023-01-13 11:12:11 +0100

    realizando

---
+++ day_4/string.py
@@ -0,0 +1,22 @@
+thirty_days_of_python= "Thirty" + " " + "Days" + " " + "Of" + " "+ "Python"
+print(thirty_days_of_python)
+
+coding_for_all= "Coding" + " " + "For" + " " + "All"
+print(coding_for_all)
+
+company = coding_for_all
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+print(company.replace("Coding"," "))
+print("Coding" in company)
+print(company.replace("Coding", "Python"))
+python_for_all = company.replace("Coding", "Python")
+print(python_for_all.replace("All", "Everyone"))
+print(company.split( ))
+marcas = "Facebook" + ", " + "Google" + ", " + "Microsoft" + ", " + "Apple" + ", " + "IBM" + ", " + "Oracle" + ", " + "Amazon"
+print(marcas.split(", "))
