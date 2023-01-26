483d57fc95f63c60c2fa347a14034d1839d6048f
Asier55555 <amartre089@m.educaand.es>
2023-01-26 09:14:34 +0100

    en proceso casi terminado

--- day_5/lists.py
+++ day_5/lists.py
@@ -13,3 +13,25 @@ it_companies.append("Acer")
 print(it_companies)
 it_companies.insert(4, "HP")
 print(it_companies)
+it_companies[1] = it_companies[1].upper()
+print(it_companies)
+print("#, ".join(it_companies))
+print("Facebook" in it_companies)
+it_companies.sort(reverse=False)
+print(it_companies)
+it_companies.reverse()
+print(it_companies)
+print(it_companies[3:9])
+print(it_companies[0:6])
+print(it_companies[0:4]+ it_companies[5:9])
+print(it_companies[1:])
+it_companies4 = it_companies[:4]+ it_companies[5:]
+print(it_companies4)
+print(it_companies[:8])
+print(it_companies.clear())
+del it_companies
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+front_back = front_end + back_end
+print(front_back)
+full_stuck =front_back
