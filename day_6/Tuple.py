54e9d540ee094f3fb68d909bc860306f8aa0ccec
Asier55555 <amartre089@m.educaand.es>
2023-01-27 11:11:41 +0100

    en proceso

---
+++ day_6/Tuple.py
@@ -0,0 +1,7 @@
+vacio = ()
+brothers = ("Guille", "Arabaneh", "Facundo" , "Mohamed")
+sisters = ("Lucía", "Consu", "María de las Piedades", "Amarguras")
+siblins = brothers + sisters
+print(brothers)
+print(sisters)
+print(siblins)
