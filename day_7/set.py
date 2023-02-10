d19bc26cb575e2bf22e4fd0d868e5bc055baa749
Asier55555 <amartre089@m.educaand.es>
2023-02-10 10:53:01 +0100

    terminado

--- day_7/set.py
+++ day_7/set.py
@@ -12,5 +12,25 @@ print(it_companies)
 it_companies.remove("HP")
 print(it_companies)
 print("la diferencia entre remove y discard es que si el elemento no existe, remove da error mientras que discard no")
-C = A + B
-print(C)
+
+print(A.union(B))
+print(A.intersection(B))
+print(A.issubset(B))
+print(A.isdisjoint(B))
+print(A.union(B))
+print(B.union(A))
+print(A.symmetric_difference(B))
+del A
+del B
+
+ages_set = set(age)
+print(len(age))
+print(len(ages_set))
+print(len(ages_set)>len(age))
+print("""la diferencia entre string, list, tuple y set es que una string es una conjunto de caracteres que se declara entre comilla y son inmutables,
+ la listas son un conjunto de string, integers, etc que son mutables, es decir, pueden cambiarse una vez declaradas y tienen orden. Sin embargo, las tuples son como las listas pero no 
+ pueden cambiarse una vez declarados, y los sets no tienen orden.""")
+teacher= "I am a teacher and I love to inspire and teach people."
+words_teacher= set(teacher.split())
+print(words_teacher )
+print(len(words_teacher))
